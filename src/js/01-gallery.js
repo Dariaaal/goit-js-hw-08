c4e087e4409c4ceaa9670560d0151531760c67c1
Dariaaal <1406dariaal@gmail.com>
2023-01-14 00:40:00 +0200

    1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+const imagesMarkup = createImagesMarkup(galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend', imagesMarkup);
+
+function createImagesMarkup(galleryItems){
+    return galleryItems.map(({preview, original, description})=>{
+        return `<a class='gallery__link' href='${original}'>
+        <img  class='gallery__image' src ='${preview}' alt ='${description}'>
+        </a>`;}).join('');
+}
+
+const gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+});
